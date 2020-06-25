8ea51d7ba7dc80e728836bf9e257ec7e75946579
Adrian Mak <damarkian@gmail.com>
2020-06-26 05:14:59 +0800

    Added helloqubit files

--- src/python/orquestra/helloqubit.py
+++ src/python/orquestra/helloqubit.py
@@ -1,22 +1,5 @@
-"""
-This module saves a welcome message.
-"""
-
 import json
 import cirq
-
-
-def welcome():
-    message = "Welcome to Orquestra!"
-
-    message_dict = {}
-    message_dict["message"] = message
-    message_dict["schema"] = "message"
-
-    with open("welcome.json",'w') as f:
-        f.write(json.dumps(message_dict, indent=2)) # Write message to file as this will serve as output artifact
-
-
 
 def helloqubit():
     # Pick a qubit.
@@ -35,5 +18,3 @@ def helloqubit():
     result = simulator.run(circuit, repetitions=20)
     print("Results:")
     print(result)
-
-
