82e7219457c2bba7c3d1a3674edf95c24c16049a
Adrian Mak <damarkian@gmail.com>
2020-06-26 15:45:27 +0800

    updated helloworld.py

--- src/python/orquestra/helloworld.py
+++ src/python/orquestra/helloworld.py
@@ -4,20 +4,16 @@ import cirq
 def helloqubit():
     # Pick a qubit.
     qubit = cirq.GridQubit(0, 0)
-
     # Create a circuit
     circuit = cirq.Circuit(
         cirq.X(qubit)**0.5,  # Square root of NOT.
         cirq.measure(qubit, key='m')  # Measurement.
     )
-    print("Circuit:")
-    print(circuit)
-
     # Simulate the circuit several times.
     simulator = cirq.Simulator()
     result = simulator.run(circuit, repetitions=20)
-    print("Results:")
-    print(result)
+    output = [circuit, result]
+    return output
 
 def hellocircuit():
     q0, q1 = cirq.LineQubit.range(2)
